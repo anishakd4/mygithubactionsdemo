1d2991e3b2658289910781059c76aa748bf335f8
Anish Kumar Dubey <anishakd4@gmail.com>
2020-06-08 10:28:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-function mooncakeSays(message) {
-  let mooncake = '(o.o)';
+// function mooncakeSays(message) {
+//   let mooncake = '(o.o)';
 
-  if ( !message ) {
-    return `${mooncake} chookity?`;
-  }
+//   if ( !message ) {
+//     return `${mooncake} chookity?`;
+//   }
+
+//   return `${mooncake} ${message}`;
+// }
+
+// module.exports.mooncakeSays = mooncakeSays;
+
+function mooncakeSays(message) {
+  let mooncake = '(0.o)';
+
+  if ( !message ) mooncake
 
   return `${mooncake} ${message}`;
 }
